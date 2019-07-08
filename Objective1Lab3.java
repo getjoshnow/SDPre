3a378240e1c7b0cd8ac6a0cb8d3884b0bb1dd3be
Josh <robbins@Joshs-MBP.hsd1.co.comcast.net>
2019-07-07 19:36:57 -0600

    complete Objective1Lab3

---
+++ Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String[] args){
+    System.out.println("Name : Joshua Robbins");
+    System.out.println("Age : 34");
+    System.out.println("State : Colorado");
+  }
+  }
