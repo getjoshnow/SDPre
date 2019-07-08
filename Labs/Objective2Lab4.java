8f24ed9b5ef28ca62d116118b2a9dbd40b3666cc
Josh <robbins@Joshs-MBP.hsd1.co.comcast.net>
2019-07-07 20:08:35 -0600

    complete Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,8 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1 = 2, num2 = 3, num3 = 99, average = 0;
+
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
