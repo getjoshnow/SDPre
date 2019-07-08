893dbb7c5288344307ab273daec667600236c078
Josh <robbins@Joshs-MBP.hsd1.co.comcast.net>
2019-07-07 19:59:03 -0600

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1 = "Ice Creame", food2 = "Banana", food3 = "Frappe";
+
+    System.out.println("My Favorite Foods Are:");
+    System.out.println("1. "+food1);
+    System.out.println("2. "+food2);
+    System.out.println("3. "+food3);
+  }
+}
