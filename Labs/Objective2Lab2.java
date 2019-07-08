cd7a15a517fe0fb9bb50d2416c2ea8de4ee2aaf6
Josh <robbins@Joshs-MBP.hsd1.co.comcast.net>
2019-07-07 19:53:01 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    System.out.print("Name: " +name+ " " );
+    System.out.print("Age: " +age+ " ");
+    System.out.println("State: "+state);
+
+  }
+}
