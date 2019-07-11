0ffa61c302097fd4e2fa8fb270c0b63ab3d18deb
Josh <robbins@Joshs-MacBook-Pro.local>
2019-07-11 16:44:04 -0600

    GasRecDrill done.

---
+++ JavaTheHardWay/GasolineReceiptDrill.java
@@ -0,0 +1,20 @@
+
+ public class GasolineReceipt {
+ public static void main( String[] args ) {
+System.out.println("+------------------------+");
+System.out.println("|                        |");
+System.out.println("|      WAWA G STORE      |");
+System.out.println("|                        |");
+System.out.println("|     2015-03-29 04:38PM |");
+System.out.println("|                        |");
+System.out.println("|  Gallons:       10.000 |");
+System.out.println("|                        |");
+System.out.println("|  Price/gallon: $ 3.899 |");
+System.out.println("|                        |");
+System.out.println("| Fuel total: $ 38.99    |");
+System.out.println("|                        |");
+System.out.println("+------------------------+");
+
+}
+
+}
