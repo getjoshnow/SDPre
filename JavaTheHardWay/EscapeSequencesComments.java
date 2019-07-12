c73726b893e21b43f4451a8bb502882972936562
Josh <robbins@Joshs-MBP.hsd1.co.comcast.net>
2019-07-11 21:00:30 -0600

    EscSeqComments done

---
+++ JavaTheHardWay/EscapeSequencesComments.java
@@ -0,0 +1,20 @@
+public class EscapeSequencesComments {
+
+    public static void main( String[] args ) {
+
+
+System.out.println( "      Learn        Java     ");
+System.out.println( "                   the      ");
+System.out.println( "           Hard        Way ");
+System.out.print( "\t   Learn Java the \"Hard\" Way!\n" );
+System.out.println( "Hello  ");
+System.out.println( "Jelly  ");
+System.out.println( "Hard to believe, eh?");
+System.out.println( "Surprised? /* abcde */ Or what did you expect?");
+System.out.println( "\\ // -=- \\ //" );
+System.out.println( "\\\\ \\\\\\ \\\\\\\\" );
+System.out.println( "I hope you understand \"escape sequences\" now.  ");
+
+    }
+
+}
