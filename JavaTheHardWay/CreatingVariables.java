1ab0cf4d2ce0c5d14f87b60b9bb0fa3da9d6a317
Josh <robbins@Joshs-MBP.hsd1.co.comcast.net>
2019-07-11 21:17:33 -0600

    CreatVarDrill

---
+++ JavaTheHardWay/CreatingVariables.java
@@ -0,0 +1,35 @@
+public class CreatingVariables {
+      public static void main( String[] args ) {
+     int x, y, age, newInt;
+     double seconds, e, checking, newDouble;
+     String firstName, lastName, title, newString, newString2;
+
+     x = 10;
+     y = 400;
+     age = 39;
+     newInt = 0;
+
+      seconds = 4.71;
+      e = 2.71828182845904523536;
+      checking = 1.89;
+      newDouble = 0.0;
+
+      firstName = "Graham";
+      lastName = "Mitchell";
+      title = "Mr.";
+      newString = "Jay";
+      newString2 = "Rob";
+
+System.out.println("The variable x contains " + x );
+System.out.println("The value " + y + " is stored in the variable y.");
+System.out.println("The experiment took " + seconds + " seconds.");
+System.out.println("A favorite irrational # is Euler's number: " + e );
+System.out.println("Hopefully you have more than $" + checking + "!" );
+System.out.println("My name's " + title + " " + firstName + lastName );
+System.out.println("My name's " + newString + " my number: " + newInt);
+System.out.println("My name's " + newString2 + " my number: " + newDouble );
+
+
+    }
+
+}
