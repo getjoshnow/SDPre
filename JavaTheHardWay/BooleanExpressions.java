30ff8b49e819a315dc470f22d2e4199a7fb51936
Josh <robbins@Joshs-MBP.hsd1.co.comcast.net>
2019-07-12 01:19:11 -0600

    BooleanExpDrill

---
+++ JavaTheHardWay/BooleanExpressions.java
@@ -0,0 +1,49 @@
+package Testing;
+
+import java.util.Scanner;
+public class BooleanExpressions {
+public static void main( String[] args ) {
+Scanner keyboard = new Scanner(System.in);
+boolean a, b, c, d, e, f;
+double x, y;
+
+System.out.print( "Give me two numbers. First: " );
+x = keyboard.nextDouble();
+System.out.print( "Second: " );
+y = keyboard.nextDouble();
+
+a=(x< y);
+b=(x<=y);
+c=(x==y);
+d=(x!=y);
+e=(x> y);
+f=(x>=y);
+
+System.out.println(x+"isLESSTHAN"+y+":"+a);
+System.out.println( x+"isLESSTHAN/EQUALTO"+y+":"+b);
+System.out.println( x+"isEQUALTO"+y+":"+c);
+System.out.println( x+"isNOTEQUALTO"+y+":"+d);
+System.out.println( x+"isGREATERTHAN"+y+":"+e);
+System.out.println( x+ "\" is GREATER THAN / EQUALTO\""+y+"\":\""+f);
+System.out.println();
+
+System.out.println( x+ "isLESSTHAN/EQUALTO"+y+":"+b);
+System.out.println( x+"isEQUALTO"+y+":"+c);
+System.out.println( !(x < y) + " " + (x >= y) );
+System.out.println( !(x <= y) + " " + (x > y) );
+System.out.println( !(x == y) + " " + (x != y) );
+System.out.println( !(x != y) + " " + (x == y) );
+System.out.println( !(x > y) + " " + (x <= y) );
+System.out.println( !(x >= y) + " " + (x < y) );
+
+// less than is <
+// greater than is >
+//!= not equals
+//== equals
+//<= less than or equals
+//>= greater than or equals
+
+
+}
+
+}
