3f4384209f62f82b71b09fbdde694f4ff466b6b5
Josh <robbins@Joshs-MBP.hsd1.co.comcast.net>
2019-07-11 21:04:54 -0600

    EscSeqComDrill done

--- JavaTheHardWay/EscapeSequencesComments.java
+++ JavaTheHardWay/EscapeSequencesComments.java
@@ -3,17 +3,17 @@ public class EscapeSequencesComments {
     public static void main( String[] args ) {
 
 
-System.out.println( "      Learn        Java     ");
-System.out.println( "                   the      ");
-System.out.println( "           Hard        Way ");
-System.out.print( "\t   Learn Java the \"Hard\" Way!\n" );
-System.out.println( "Hello  ");
-System.out.println( "Jelly  ");
-System.out.println( "Hard to believe, eh?");
-System.out.println( "Surprised? /* abcde */ Or what did you expect?");
-System.out.println( "\\ // -=- \\ //" );
-System.out.println( "\\\\ \\\\\\ \\\\\\\\" );
-System.out.println( "I hope you understand \"escape sequences\" now.  ");
+System.out.println( "Learn        Java\n     "
+                    +"\tthe \n     "
+                    +"Hard        Way "
+                    +"\t   Learn Java the \"Hard\" Way!\n"
+                    +"Hello  \n"
+                    +"Jelly  \n"
+                    +"Hard to believe, eh?\n"
+                    +"Surprised? /* abcde */ Or what did you expect?\n"
+                    +"\\ // -=- \\ //\n"
+                    +"\\\\ \\\\\\ \\\\\\\\\n"
+                    +"I hope you understand \"escape sequences\" now.  ");
 
     }
 
